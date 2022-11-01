891d60b1a6d2f1b53e29203d2019798932b2cc78
diego <guatemaladiegojose@gmail.com>
2022-11-01 00:17:23 -0400

    commit tree dff2170868de55fde7276490374e90b8dc5997d0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+// findMatchin- ths function takes an array of driver's names and a string as arguments, 
+// and returns the matching list of drivers.The function should be case insensitive
+
+// drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, NombreIntroducido) {
+  return drivers.filter(function (conductor) { return conductor.toLowerCase() === NombreIntroducido.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, NombrePerson) {
+    return drivers.filter(function(conductor) {
+      return conductor.toLowerCase().substring(0, NombrePerson.length) === NombrePerson.toLowerCase().substring(0, NombrePerson.length)
+      
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter (
+      
+      function (driverOfDrivers) {
+        return driverOfDrivers.name === string
+      })
+  
+    
+  }
+  console.log(matchName(drivers, "Sally"));
+  
+
+  
